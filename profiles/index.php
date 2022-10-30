338bcf95ec71bf3eab1d06533c8b697bbca4c302
jmm51 <jmm51@pc8-077-l.cs.st-andrews.ac.uk>
2022-10-30 18:02:46 +0000

    loader

--- profiles/index.php
+++ profiles/index.php
@@ -14,8 +14,6 @@
 </head>
 <body>
 
-    <?php session_start(); ?>
-
     <div class="header">
 
         <h1>bandersnatch~ed</h1>
@@ -37,6 +35,9 @@
             
 
             <div class="buttons">
+
+                <div class="spin hide" id="spinner"></div>
+
                 <button onclick="generate(true)"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="25" style="position:relative;top:3px;"><path fill="var(--primary-text)" d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg></button>
                 <button onclick="generate(false)"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" height="25" style="position:relative;top:3px;"><path fill="var(--primary-text)" d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/></svg></button>
             </div>
