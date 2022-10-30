82f735061e57e7d34929b56d5904e99c8f853864
cc398 <cc398@pc8-076-l.cs.st-andrews.ac.uk>
2022-10-30 18:47:14 +0000

    optimisations

--- profiles/scripts/generator.php
+++ profiles/scripts/generator.php
@@ -10,32 +10,34 @@
     $findTopia = json_decode($_POST['data'], true)["topia"];
 
     function costFunction(int $found, int $target){
-        // where $found is the value of the new show, and $factor is the user preference
+        // where $found is the value of the new show, and $target is the user preference
         $cost = pow($found - $target, 2);
         return $cost;
     }
 
-    $sql_num_of_shows = "SELECT * FROM shows";
-    $result = mysqli_query($conn, $sql_num_of_shows);
-    $total_shows = mysqli_num_rows($result);
-     
-    // Find best show ID
+    // find best show ID
     $best_show_ID = 0;
     $lowest_cost = 126;
-    for ($index=1; $index<=$total_shows; $index++){
-        $sql = "SELECT `ID`, `horror`, `fantasy`, `time`, `family`, `topia` FROM shows WHERE `ID` = ".$index;
-        $result = mysqli_query($conn, $sql);
-        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-        
-        $cost = 0;
-        $cost += costFunction($row["horror"], $findHorror);
-        $cost += costFunction($row["fantasy"], $findFantasy);
-        $cost += costFunction($row["time"], $findTime);
-        $cost += costFunction($row["family"], $findFamily);
-        $cost += costFunction($row["topia"], $findTopia);
-        if ($cost < $lowest_cost){
-            $lowest_cost = $cost;
-            $best_show_ID = $row["ID"];
+
+    $sql = "SELECT `ID`, `horror`, `fantasy`, `time`, `family`, `topia` FROM shows";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result)>0){
+        while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+            $cost = 0;
+            $sumFound = array_sum($row) - $row["ID"];
+            $sumTarget = $findHorror + $findFantasy + $findTime + $findFamily + $findTopia;
+            $cost = costFunction($sumFound, $sumTarget);
+            /* $cost += costFunction($row["horror"], $findHorror);
+            $cost += costFunction($row["fantasy"], $findFantasy);
+            $cost += costFunction($row["time"], $findTime);
+            $cost += costFunction($row["family"], $findFamily);
+            $cost += costFunction($row["topia"], $findTopia); */
+            if ($cost < $lowest_cost){
+                $best_show_ID = $row["ID"];
+                $lowest_cost = $cost;
+                // break;
+            }
         }
     }
 
@@ -46,4 +48,5 @@
 
     // send all data back
     echo implode("*",$info);
+    
 ?>
